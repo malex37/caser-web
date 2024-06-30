8237ca5a8a9e08636821e47b59483d176b46e300
malex <malex@iammalex.com>
2024-06-29 23:17:27 -0700

    Change folder widget refresh button and alignmnet

--- src/app/(authenticated)/dashboard/page.tsx
+++ src/app/(authenticated)/dashboard/page.tsx
@@ -6,6 +6,7 @@ import ListCard from '@components/molecules/ListCard';
 import ModalContainer from '@components/molecules/ModalContainer';
 import { Folder } from '@models/DBSchemas/Folders';
 import { useEffect, useState } from 'react';
+import { ArrowPathIcon } from '@heroicons/react/24/outline';
 
 export default function Dashboard(): JSX.Element {
 
@@ -13,8 +14,8 @@ export default function Dashboard(): JSX.Element {
   const [folders, setFolders] = useState([] as Folder[]);
 
   const fetchFolders = async () => {
-      const r = await GetFolders();
-      setFolders(r);
+    const r = await GetFolders();
+    setFolders(r);
   };
 
   const submitRefresh = (e: any) => {
@@ -23,7 +24,7 @@ export default function Dashboard(): JSX.Element {
   }
 
   useEffect(() => {
-      fetchFolders();
+    fetchFolders();
   }, [])
   return (
     <div className='w-full h-full p-0 flex flex-col'>
@@ -32,17 +33,17 @@ export default function Dashboard(): JSX.Element {
           Welcome to your dashboard! 🏡
         </div>
         {/* TODO: Move this to a component */}
-        <div id="dashboard-menu" className='join gap-2'>
+        <div id="dashboard-menu" className='join gap-2 p-2'>
           <ModalContainer buttonText='New Folder' modalId='new-folder-join' additionalClassNames='btn join-item'>
-            <NewFolder modalId='new-folder-join'/>
+            <NewFolder modalId='new-folder-join' />
           </ModalContainer>
-          <button className='btn join-item'>Some other function</button>
         </div>
       </div>
       <div className='w-1/3 shadow shadow-moonstone m-3'>
-        <div className='flex'>
-          <h2 className='p-3 text-xl border border-x-transparent border-y-transparent border-b-neutral-300'>Folders</h2>
-          <button className='w-auto btn btn-accent' onClick={submitRefresh}>R</button>
+        <div className='flex w-full border border-x-transparent border-y-transparent border-b-neutral-300 p-2'>
+            <h2 className='p-3 text-xl'>Folders</h2>
+            <div className='w-full'></div>
+            <button className='p-2 btn' onClick={submitRefresh}><ArrowPathIcon className='h-full w-auto'/></button>
         </div>
         <ScrollArea>
           {
