c4c5d0b556eea5711a28c0caad602aabf5cb8068
malex <malex@iammalex.com>
2024-10-03 11:45:38 -0700

    Attempting to fix panning

--- src/components/preview-widgets/Image.tsx
+++ src/components/preview-widgets/Image.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { FileManager } from "@storage/FileManager";
-import { useEffect, useState, DragEvent, useCallback, useRef } from "react";
+import { useEffect, useState, DragEvent, useCallback, useRef, MouseEvent as ME } from "react";
 
 interface Point {
   x: number;
@@ -55,23 +55,6 @@ export default function ImagePreview({ file }: { file: File }) {
     setBlobUrl(blb);
   }
 
-  const panImage = () => {
-    const { context } = getCanvas();
-    context.translate(100, 0);
-    console.log('Translating');
-  }
-
-  const dragStart = (event: DragEvent<HTMLCanvasElement>) => {
-    console.log(`Drag started at x ${event.pageX}, y ${event.pageY}`);
-    const { canvas, context } = getCanvas();
-    context.setTransform(1, 0, 0, 1, 0, 0);
-    canvas.style.cursor = 'grab';
-    var ghost = new Image();
-    ghost.src = 'data:image/gif;base64,R0lGODlhAQABAIAAAAUEBAAAACwAAAAAAQABAAACAkQBADs=';
-    event.dataTransfer.setDragImage(ghost, 0, 0);
-    canvas.onmousemove = (e) => { panImage() };
-  };
-
   const getCanvas = useCallback((): { context: CanvasRenderingContext2D, canvas: HTMLCanvasElement } => {
     if (!canvasComponent.current) {
       error('No current canvas component');
@@ -96,7 +79,7 @@ export default function ImagePreview({ file }: { file: File }) {
     //const dx = xl / 2;
     const dx = x;
     const dy = y;
-    // log(`Crosshair is at (${dx}, ${dy})`);
+    log(`Crosshair [${color}] is at (${dx}, ${dy})`);
     // log(`Crosshair lengths: x ${xl}, y ${yl}`);
     context.strokeStyle = color;
     context.beginPath();
@@ -140,35 +123,38 @@ export default function ImagePreview({ file }: { file: File }) {
     viewport.x += xmove;
     viewport.y += ymove;
     log(`Target srcOrigin (${viewport.x}, ${viewport.y})`);
-    context.drawImage(imageHolder.current, viewport.x, viewport.y, canvas.width, canvas.height, 0, 0, canvas.width, canvas.height);
+    context.drawImage(imageHolder.current, viewport.x, viewport.y, canvas.width, canvas.height, xmove, ymove, canvas.width, canvas.height);
     drawCrosshair(context, realPosition.x, realPosition.y, canvas.width, canvas.height);
     markCenter(context, canvas.width, canvas.height);
   };
 
-  const mouseDown = (e: any) => {
+  const mouseDown = (event: ME<HTMLCanvasElement, MouseEvent>) => {
+    log('Mouse down');
     const { canvas } = getCanvas();
-    canvas.addEventListener("mousemove", mouseMove);
+    if (!canvasComponent.current) {
+      log('No canvas to attach event');
+      return;
+    }
+    canvasComponent.current.addEventListener("mousemove", (e) => mouseMove(event));
   };
 
-  const mouseUp = () => {
+  const mouseUp = (event: ME<HTMLCanvasElement, MouseEvent>) => {
     const { canvas } = getCanvas();
-    canvas.removeEventListener("mousemove", mouseMove);
+    log('Mouse up');
+    canvas.removeEventListener("mousemove", () => mouseMove(event));
   }
 
   // Needs to be callbacked so it remains across re-renders and removelistener cand identify
   // when looking through the attached listeners
-  const mouseMove = useCallback((e: any) => {
+  const mouseMove = useCallback((e: ME<HTMLCanvasElement, MouseEvent>) => {
+    log('Mouse move');
     if (e.currentTarget) {
-      const { left, top } = e.currentTarget.getBoundingClientRect();
-      realPosition.x = e.clientX - left;
-      realPosition.y = e.clientY - top;
-      if (previousCoords.x === 0 && previousCoords.y === 0) {
-        previousCoords.x = realPosition.x;
-        previousCoords.y = realPosition.y;
-      }
+      log(`Move triggered on ${e.currentTarget.id}`);
+      realPosition.x = e.nativeEvent.offsetX;
+      realPosition.y = e.nativeEvent.offsetY;
       drawOnCanvas();
-      previousCoords.x = e.clientX - left;
-      previousCoords.y = e.clientY - top;
+      previousCoords.x = realPosition.x;
+      previousCoords.y = realPosition.y;
     }
   }, []);
 
@@ -182,9 +168,8 @@ export default function ImagePreview({ file }: { file: File }) {
         <canvas
           id="image-canvas"
           ref={canvasComponent}
-          className="w-full h-full"
-          onMouseDown={mouseDown}
-          onMouseUp={mouseUp}
+          onMouseDown={(e) => mouseDown(e)}
+          onMouseUp={(e) => mouseUp(e)}
           onMouseLeave={mouseUp}
         ></canvas>
       }
